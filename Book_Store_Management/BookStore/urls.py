4a667fefeca13f3af990b52dac506b50d5980090
Sakthivel Murugesan <114555157+SakthivelM7@users.noreply.github.com>
2024-03-06 14:13:05 +0530

    Add files via upload

---
+++ Book_Store_Management/BookStore/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from BookStore import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('addauthor', views.add_author, name='addauthor'),
+    path('displayauthor', views.display_author, name='displayauthor'),
+    path('deleteauthor/<int:id>', views.delete_author, name='deleteauthor'),
+    path('editauthor/<int:id>', views.edit_author, name='editauthor'),
+    path('addbook', views.add_book, name='addbook'),
+    path('displaybookdetails', views.display_book, name='displaybook'),
+    path('editbook/<int:id>', views.edit_book, name='editbook'),
+    path('deletebook/<int:id>', views.delete_book, name='deletebook'),
+
+    #login system urls
+    path('login', views.login_fun, name='login'),
+    path('register', views.register_fun, name='register'),
+    path('logout', views.logout_fun, name='logout')
+]
